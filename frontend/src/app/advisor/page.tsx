a7a25c11310d173e11ce7d010cde932f0de11a76
abdulah-x <abdullah.asad2001@gmail.com>
2026-07-19 07:15:06 +0500

    feat(advisor): create advisor page route serving AI chat interface

---
+++ frontend/src/app/advisor/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import AppLayout from "@/components/layout/AppLayout";
+import ProtectedRoute from "@/components/auth/ProtectedRoute";
+import AdvisorChat from "@/components/advisor/AdvisorChat";
+
+export default function AdvisorPage() {
+  return (
+    <ProtectedRoute>
+      <AppLayout>
+        <div className="h-[calc(100vh-73px)] flex flex-col">
+          <AdvisorChat />
+        </div>
+      </AppLayout>
+    </ProtectedRoute>
+  );
+}
